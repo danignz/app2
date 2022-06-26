54a39123040243974b0abdf4b9635d3fc8e3954e
elisa <bascongarcia@gmail.com>
2022-06-26 15:48:28 +0200

    question routes Done

---
+++ routes/questions.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const Question = require("../models/Question");
+
+router.get("/", async (req, res, next) => {
+    try {
+      const questions = await Question.find({});
+      res.render("/questions", { questions });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router.get("/create", (req, res, next) => {
+    res.render("questions/new-question");
+  });
+
+router.post("/create", async (req, res, next) => {
+    const { question, ccorrect_answer, incorrect_answers, category, difficulty, question_img, isVisible } = req.body;
+    try { 
+       await Question.create({ question, ccorrect_answer, incorrect_answers, category, difficulty, question_img, isVisible });
+       res.redirect("/questions");
+    }  catch (error) {
+       next(error);
+       res.redirect("/questions/create"); 
+    } 
+});
+
+module.exports = router;
