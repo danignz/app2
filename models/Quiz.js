aae2a28056fdffcf2cb9052f00aa88359e65256e
elisa <bascongarcia@gmail.com>
2022-06-28 20:01:26 +0200

    Closed #21

---
+++ models/Quiz.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+const quizSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required"],
+  },
+  difficulty: {
+    type: String,
+    enum: ["BASIC", "MEDIUM", "ADVANCE"],
+    required: [true, "Difficulty is required"],
+  },
+  category: {
+    type: String,
+    enum: ["JAVASCRIPT", "CSS", "HTML"],
+    required: [true, "Category is required"],
+  },
+  points_required: {
+    type: Number,
+    required: [true, "Poinst are required"],
+  },
+  question: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Question',
+    required: [true, "Questions are required"],
+  },
+  num_questions: {
+    type: Number,
+    required: [true, "Number of questions are required"],
+  },
+  quiz_img: {
+    type: String,
+    default: "/images/quiz/default.jpg",
+  },
+  isVisible: {
+    type: Boolean,
+  },
+});
+
+const Quiz = model("Quiz", quizSchema);
+
+module.exports = Quiz;
